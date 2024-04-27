1ba97d4417e55bfd863e0672c65af94337faad4d
satyajay01 <satyajaydibya0@gmail.com>
2024-04-27 23:53:31 +0530

    Project setup and files structure

---
+++ src/components/TitileCards/TitileCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TitileCards.css'
+
+const TitileCards = () => {
+  return (
+    <div className='titileCards'>TitileCards</div>
+  )
+}
+
+export default TitileCards
