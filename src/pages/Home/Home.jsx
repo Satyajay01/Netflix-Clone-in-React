ae9575da679d249175e92eb9aa8e46ddde70c3b9
satyajay01 <satyajaydibya0@gmail.com>
2024-04-28 16:31:23 +0530

    Hero Section Created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png"
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar/>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Instanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn"><img src={play_icon} alt="" />Play</button>
+            <button className="btn dark-btn"><img src={info_icon} alt="" />More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
